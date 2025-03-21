66d1325281211fb615c0f9c8f1ca5cccaae5fabb
Steven Alvarez <stevounit@gmail.com>
2025-03-21 00:35:58 -0400

    Update 03/20/25 -V8

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,39 +1,39 @@
 import { defineData } from "@aws-amplify/backend-data";
 import { a } from "@aws-amplify/data-schema";
 
-const schema = a.schema({
-  User: a.model({
-    userId: a.id(),
-    name: a.string(),
-    role: a.string(),
-    groupId: a.string(),
-    group: a.belongsTo("Group", "groupId"),
-  }),
+const schema = a
+  .schema({
+    User: a.model({
+      userId: a.id(),
+      name: a.string(),
+      role: a.string(),
+      groupId: a.string(),
+      group: a.belongsTo("Group", "groupId"),
+    }),
 
-  Group: a.model({
-    groupId: a.id(),
-    roleName: a.string(),
-    users: a.hasMany("User", "groupId"),
-  }),
+    Group: a.model({
+      groupId: a.id(),
+      roleName: a.string(),
+      users: a.hasMany("User", "groupId"),
+    }),
 
-  Conversation: a.model({
-    conversationId: a.id(),
-    participants: a.string().array(),
-    messages: a.hasMany("Message", "messageId"),
-    lastMessage: a.string(),
-    lastUpdated: a.datetime(),
-  }),
+    Conversation: a.model({
+      conversationId: a.id(),
+      participants: a.string().array(),
+      messages: a.hasMany("Message", "messageId"),
+      lastMessage: a.string(),
+      lastUpdated: a.datetime(),
+    }),
 
-  Message: a.model({
-    messageId: a.id(),
-    conversationId: a.string(),
-    conversation: a.belongsTo("Conversation", "conversationId"),
-    senderId: a.string(),
-    text: a.string(),
-    timestamp: a.datetime(),
-  }),
-});
-
-// No authorization and no required fields here
+    Message: a.model({
+      messageId: a.id(),
+      conversationId: a.string(),
+      conversation: a.belongsTo("Conversation", "conversationId"),
+      senderId: a.string(),
+      text: a.string(),
+      timestamp: a.datetime(),
+    }),
+  })
+  .authorization([a.allow.public()]); // Global authorization rule
 
 export const data = defineData({ schema });
